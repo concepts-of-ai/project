1f974e439e478912c27ce94dd509e58e9c366172
Eli Morse <elimorse15+github@gmail.com>
2023-04-13 13:29:56 -0500

    got input to workish

--- MiniMaxOpening.cs
+++ MiniMaxOpening.cs
@@ -1,15 +1,69 @@
 namespace minimaxopening;
 using morrisf;
+using node;
+using System.IO;
 
 class MiniMaxOpening
 {
     public static void MiniMaxOpeningRun(string[] args)
     {
-        if (args.Count() != 3) {
+        if (args.Count() != 3)
+        {
             Console.WriteLine("\n ***** Not enough input parameters -- try again.\n");
+            return;
         }
+
+        StreamReader? reader;
+        try
+        {
+            reader = new StreamReader(args[0]);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("\n ***** Invalid input file.\n");
+            return;
+        }
+
+        StreamWriter? writer;
+        try
+        {
+            writer = new StreamWriter(args[1]);
+        } 
+        catch (Exception) 
+        {
+            Console.WriteLine("\n ***** Invalid output file.\n");
+            return;
+        }
+
+        int depth;
+        try 
+        {
+            depth = Int32.Parse(args[2]);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("\n ***** Invalid depth value.\n");
+            return;
+        }
+
+        // read input board from input file
+        Node treeRoot;
+        string? boardAsString = reader.ReadLine();
+        if (boardAsString != null && boardAsString.Length! != 0)
+        {
+            board.BoardState state = new board.BoardState(boardAsString);
+            treeRoot = new Node(state);
+        } else {
+            Console.WriteLine("\n ***** Invalid input file value.\n");
+            return;
+        }
+
+        // compute minimax
+
+
+        // write output to output file
+
     }
 
-    
 
 }
