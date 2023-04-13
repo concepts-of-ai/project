24bd41ae9130c2550c48a344e67e584a4fa9c356
JacobCharvat <jacob.charvat12@gmail.com>
2023-04-13 10:07:06 -0500

    Copied files

---
+++ part3/MorrisF.cs
@@ -0,0 +1,129 @@
+using board;
+
+public class MorrisF
+{
+    private const int NumberOfPositions = 24;
+
+    public List<BoardState> GenerateMovesOpening(BoardState board)
+    {
+        return GenerateAdd(board);
+    }
+
+    public List<BoardState> GenerateMovesMidgameEndgame(BoardState board)
+    {
+        if (board.StateCount(State.W) == 3) return GenerateHopping(board);
+        else return GenerateMove(board);
+    }
+
+    private List<BoardState> GenerateAdd(BoardState board)
+    {
+        var states = new List<BoardState>();
+        for (int location = 0; location < NumberOfPositions; location++)
+        {
+            if (board.IsEmptyPosition(location))
+            {
+                var tempBoard = board.Copy();
+                tempBoard.SetState(location, State.W);
+                if (CloseMill(location, tempBoard)) GenerateRemove(tempBoard, states);
+                else states.Add(tempBoard);
+            }
+        }
+        return new List<BoardState>();
+    }
+
+    private List<BoardState> GenerateMove(BoardState board)
+    {
+        var states = new List<BoardState>();
+        for (int location = 0; location < NumberOfPositions; location++)
+        {
+            if (board.GetState(location) == State.W)
+            {
+                var neighbors = BoardLayout.GetNeighbors(location);
+                for (int position = 0; position < neighbors.Count; position++) 
+                {
+                    if (board.IsEmptyPosition(position))
+                    {
+                        var tempBoard = board.Copy();
+                        tempBoard.SetState(location, State.x);
+                        tempBoard.SetState(position, State.W);
+                        if (CloseMill(position, tempBoard)) GenerateRemove(tempBoard, states);
+                        else states.Add(tempBoard); 
+                    }
+                }
+            }
+        }
+        return new List<BoardState>();
+    }
+
+    private List<BoardState> GenerateHopping(BoardState board)
+    {
+        var states = new List<BoardState>();
+        for (int location = 0; location < NumberOfPositions; location++) {
+            if (board.GetState(location) == State.W)
+            {
+                for (int location2 = 0; location2 < NumberOfPositions; location2++)
+                {
+                    if (board.IsEmptyPosition(location2))
+                    {
+                        var tempBoard = board.Copy();
+                        tempBoard.SetState(location, State.x);
+                        tempBoard.SetState(location2, State.W);
+                        if (CloseMill(location2, tempBoard)) GenerateRemove(tempBoard, states);
+                        else states.Add(tempBoard);
+                    }
+                }
+            }
+        }
+        return new List<BoardState>();
+    }
+
+    private void GenerateRemove(BoardState board, List<BoardState> states)
+    {
+        var length = states.Count;
+        for (int location = 0; location < NumberOfPositions; location++)
+        {
+            if (board.GetState(location) == State.B)
+            {
+                if (!CloseMill(location, board))
+                {
+                    var tempBoard = board.Copy();
+                    tempBoard.SetState(location, State.x);
+                    states.Add(tempBoard);
+                }
+            }
+        }
+        if (states.Count == length) 
+        {
+            for (int location = 0; location < NumberOfPositions; location++)
+            {
+                if (board.GetState(location) == State.B)
+                {
+                    var tempBoard = board.Copy();
+                    tempBoard.SetState(location, State.x);
+                    states.Add(tempBoard);
+                }
+            }
+        }
+
+    }
+
+    private List<int> Neighbors(int location)
+    {
+        return BoardLayout.GetNeighbors(location);
+    }
+
+    private bool CloseMill(int location, BoardState board)
+    {   // refactor to make this more efficent later
+        State pos = board.GetState(location);
+        for (int i = 0; i < BoardLayout.NumberOfMills(); i++)
+        {
+            var (a, b, c) = BoardLayout.GetMill(i);
+            State s1 = board.GetState(a);
+            State s2 = board.GetState(b);
+            State s3 = board.GetState(c);
+            if (s1 == pos && s2 == pos && s3 == pos) return true;
+        }
+        return false;
+    }
+
+}
---
+++ part4/BoardState.cs
@@ -0,0 +1,155 @@
+namespace board;
+
+public enum State
+{ 
+    // 2 bits determine state of one possition (White, Black, Empty)
+    x = 0b00,
+    W = 0b01,
+    B = 0b10,
+    Check = 0b11,
+}
+
+public class BoardState
+{
+    // frist 16 possitions in first block and remaining 8 in second block (24 total)
+    const int BlockOne = 0;
+    const int BlockOneCapacity = 16;
+    const int BlockTwo = 1;
+    const int BlockTwoCapacity = 8;
+    const int TotalCapacity = 24;
+
+    // array of two uint32 to hold board info
+    uint[] board = new uint[2];
+
+    public BoardState(string input)
+    {
+        board[0] = 0;
+        board[1] = 0;
+        SetBoard(input);
+    }
+
+    // copy contructor
+    private BoardState(BoardState input)  
+    {
+        board[0] = input.GetBlock(0);
+        board[1] = input.GetBlock(1);
+    }
+
+    public BoardState Copy()
+    {
+        return new BoardState(this);
+    }
+
+    public void SetBoard(string input)
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+            var state = ParseState(input[i].ToString());
+            SetState(i, state);
+        }
+    }
+
+    public String GetBoard()
+    {
+        String result = "";
+        for (int i = 0; i < TotalCapacity; i++)
+        {
+            result += GetState(i).ToString();
+        }
+        return result;
+    }
+
+    public override string ToString()
+    {
+        return this.GetBoard();
+    }
+
+    public void SetState(int pos, State state)
+    {
+        var (offset, block) = FindBlockAndOffset(pos);
+        uint tempBlock = ClearPosition(offset, block);
+
+        // if settings state to empty, return early
+        if (state == State.x)
+        {
+            SetBlock(block, tempBlock);
+            return;
+        }
+
+        // move the state you are setting to correct position
+        uint s = (uint)state << offset;
+        // use logical OR to add it tempblock
+        tempBlock = tempBlock | s;
+        SetBlock(block, tempBlock);
+    }
+
+    public State GetState(int pos)
+    {
+        var (offset, block) = FindBlockAndOffset(pos);
+
+        // bitwise shift so that check bits line up with pos of block
+        uint tempBlock = GetBlock(block);   // get a copy of the block
+        tempBlock >>= offset;   // offset block so that position is lowest digits
+        uint checkResult = tempBlock & (int)State.Check;   // logic AND to get result
+        return (State)checkResult; // convert to State
+    }
+
+    public bool IsEmptyPosition(int pos) 
+    {
+        return GetState(pos) == State.x;
+    }
+
+    public int StateCount(State state)
+    {
+        int count = 0;
+        for (int location = 0; location < TotalCapacity; location++)
+        {
+            if (GetState(location) == state) count++;
+        }
+        return count;
+    }
+
+    uint GetBlock(int block)
+    {
+        return board[block];
+    }
+
+    void SetBlock(int block, uint val)
+    {
+        board[block] = val;
+    }
+
+    uint ClearPosition(int offset, int block)
+    {
+        // magic 🪄
+        uint checkBits = (uint)State.Check;
+        checkBits <<= offset;
+        uint mask = ~checkBits;
+        uint tempBlock = GetBlock(block);
+        return tempBlock & mask;
+    }
+
+    State ParseState(String item)
+    {
+        return (State)Enum.Parse(typeof(State), item, true);
+    }
+
+    (int offset, int block) FindBlockAndOffset(int pos)
+    {
+
+        var block = BlockOne;
+        if (pos < 0 || pos > 23)
+        {
+            Console.Write("{0} invalid position", pos);
+        }
+
+        if (pos >= BlockOneCapacity)
+        {
+            block = BlockTwo;
+            pos -= BlockOneCapacity;
+        }
+
+        // position * 2 (since one state is 2 bits per position)
+        return (pos * 2, block);
+    }
+}
