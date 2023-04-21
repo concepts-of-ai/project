7e4e14423092d80af332a84d499ffe5c8d22bd6f
Eli Morse <elimorse15+github@gmail.com>
2023-04-21 11:40:44 -0500

    formatting improvement

--- src/ABGame.cs
+++ src/ABGame.cs
@@ -5,17 +5,18 @@ using System.Collections.Generic;
 class ABGame : ProgramEntry
 {
     public static void Main(String[] args)
-    {   
-        var program = new ABGame();
-        program.Run(args);
+    {
+        var game = new ABGame();
+        game.Run(args);
     }
 
-    protected override (Node, Node) ComputeMinMax(BoardState state, int depth, ref long stateCounter) {
+    protected override (Node, Node) ComputeMinMax(BoardState state, int depth, ref long stateCounter)
+    {
         MorrisF morrisF = new MorrisF();
         Node root = new Node(state);
         Node tree = morrisF.GenerateMovesMidgameEndgame(root, depth, true);
 
-        var value = MiniMaxOptions.ABMaxMinMidgame(tree, -100000, 1000000,  ref stateCounter, true);
+        var value = MiniMaxOptions.ABMaxMinMidgame(tree, -100000, 1000000, ref stateCounter, true);
         tree.SetValue(value);
         Node bestChild = tree.findChildNode();
         return (root, bestChild);
--- src/MiniMaxGame.cs
+++ src/MiniMaxGame.cs
@@ -10,12 +10,13 @@ class MiniMaxGame : ProgramEntry
         game.Run(args);
     }
 
-    protected override (Node, Node) ComputeMinMax(BoardState state, int depth, ref long stateCounter) {
+    protected override (Node, Node) ComputeMinMax(BoardState state, int depth, ref long stateCounter)
+    {
         MorrisF morrisF = new MorrisF();
         Node root = new Node(state);
         Node tree = morrisF.GenerateMovesMidgameEndgame(root, depth, true);
 
-        var value = MiniMaxOptions.MaxMinMidgame(tree,  ref stateCounter, true);
+        var value = MiniMaxOptions.MaxMinMidgame(tree, ref stateCounter, true);
         tree.SetValue(value);
         Node bestChild = tree.findChildNode();
         return (root, bestChild);
